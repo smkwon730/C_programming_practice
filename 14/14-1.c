fc8b361207c37a8c9dd6fa8f3b28b85723f9178a
smkwon730 <44897282+smkwon730@users.noreply.github.com>
2021-12-07 16:03:36 +0900

    Create 14-1.c

---
+++ 14/14-1.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main(void) {
+	char* pc = NULL;
+	int i = 0;
+
+	pc = (char*)malloc(100 * sizeof(char));
+	if (pc == NULL) {
+		printf("메모리 할당 오류\n");
+		exit(1);
+	}
+	for (i = 0; i < 26; i++) {
+		pc[i] = 'a' + i;
+	}
+	pc[i] = 0;					//문자열 만들기 위해 마지막에 0 넣어줌
+	printf("%s\n", pc);
+	free(pc);
+}
